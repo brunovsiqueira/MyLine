b638615a704d037f30481ae52bafe1f29bac63a6
Bruno Siqueira <brunovsiq@gmail.com>
2018-05-15 15:40:48 -0300

    Add bottom bar

--- MyLine/app/src/main/java/ihc/unb/com/br/myline/screens/container/ContainerActivity.java
+++ MyLine/app/src/main/java/ihc/unb/com/br/myline/screens/container/ContainerActivity.java
@@ -1,13 +1,15 @@
-package ihc.unb.com.br.myline;
+package ihc.unb.com.br.myline.screens.container;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import ihc.unb.com.br.myline.R;
+
+public class ContainerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.container_activity);
     }
 }
