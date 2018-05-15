479fcfd89b0ed834123e3c850cd9c01e888bf35b
Bruno Siqueira <brunovsiq@gmail.com>
2018-05-15 14:53:42 -0300

    Initial project

---
+++ MyLine/app/src/main/java/ihc/unb/com/br/myline/MainActivity.java
@@ -0,0 +1,13 @@
+package ihc.unb.com.br.myline;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
