bebf75440aefd502528fd86126adacd3fdfb4d97
brunovsiq <brunovsiq@gmail.com>
2018-06-18 23:28:45 -0300

    Start creating restaurant screen

--- MyLine/app/src/main/java/ihc/unb/com/br/myline/myline/screens/container/restaurant/RestaurantItemActvity.java
+++ MyLine/app/src/main/java/ihc/unb/com/br/myline/myline/screens/container/restaurant/RestaurantItemActvity.java
@@ -3,13 +3,18 @@ package ihc.unb.com.br.myline.myline.screens.container.restaurant;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 
 import ihc.unb.com.br.myline.R;
 import ihc.unb.com.br.myline.myline.models.Restaurant;
 
-public class RestaurantItemActvity extends Activity {
+public class RestaurantItemActvity extends AppCompatActivity {
 
     private Restaurant restaurant;
+
+    //private CarouselView carouselView;
+
+    int[] sampleImages = {R.drawable.outback, R.drawable.outback2};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
