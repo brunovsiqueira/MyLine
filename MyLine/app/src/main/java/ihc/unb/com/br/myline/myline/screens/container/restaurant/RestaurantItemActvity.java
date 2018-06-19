72f965725829e45a17231085e481b5e0aa58370e
brunovsiq <brunovsiq@gmail.com>
2018-06-19 14:57:47 -0300

    Finish RestaurantItemActivity layout

--- MyLine/app/src/main/java/ihc/unb/com/br/myline/myline/screens/container/restaurant/RestaurantItemActvity.java
+++ MyLine/app/src/main/java/ihc/unb/com/br/myline/myline/screens/container/restaurant/RestaurantItemActvity.java
@@ -4,6 +4,11 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 import ihc.unb.com.br.myline.R;
 import ihc.unb.com.br.myline.myline.models.Restaurant;
@@ -12,7 +17,10 @@ public class RestaurantItemActvity extends AppCompatActivity {
 
     private Restaurant restaurant;
 
-    //private CarouselView carouselView;
+    private CarouselView carouselView;
+    private TextView textHour;
+    private TextView textLocale;
+    private TextView textPhone;
 
     int[] sampleImages = {R.drawable.outback, R.drawable.outback2};
 
@@ -23,5 +31,26 @@ public class RestaurantItemActvity extends AppCompatActivity {
 
         restaurant = (Restaurant) getIntent().getSerializableExtra("restaurant");
 
+        findViewItems();
+
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+
+
     }
+
+    private void findViewItems() {
+
+        carouselView = (CarouselView) findViewById(R.id.carouselView);
+
+
+    }
+
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
+
 }
