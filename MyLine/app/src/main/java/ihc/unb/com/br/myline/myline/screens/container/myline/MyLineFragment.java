be7363e117d8d3a1572d66295b78bdb7c85bea2b
Lucas Nascimento Santos Souza <lucasnascimentoss@gmail.com>
2018-06-23 15:37:42 -0300

    MyLine screen done :ok_hand:

--- MyLine/app/src/main/java/ihc/unb/com/br/myline/myline/screens/container/myline/MyLineFragment.java
+++ MyLine/app/src/main/java/ihc/unb/com/br/myline/myline/screens/container/myline/MyLineFragment.java
@@ -1,8 +1,10 @@
 package ihc.unb.com.br.myline.myline.screens.container.myline;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,7 @@ import ihc.unb.com.br.myline.R;
 public class MyLineFragment extends Fragment {
 
     View view;
+    SwipeRefreshLayout swipeLayout;
 
     @Nullable
     @Override
@@ -23,7 +26,26 @@ public class MyLineFragment extends Fragment {
 
         view = inflater.inflate(R.layout.fragment_myline, container, false);
 
+        // Getting SwipeContainerLayout
+        swipeLayout = (SwipeRefreshLayout) view.findViewById(R.id.swipeContainer);
+        // Adding Listener
+        swipeLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // To keep animation for 4 seconds
+                new Handler().postDelayed(new Runnable() {
+                    @Override public void run() {
+                        // Stop animation (This will be after 3 seconds)
+                        swipeLayout.setRefreshing(false);
+                    }
+                }, 2000); // Delay in millis
+            }
+        });
+
+        // Scheme colors for animation
+        swipeLayout.setColorSchemeResources(R.color.colorAccent);
+
         return view;
     }
-
 }
+
